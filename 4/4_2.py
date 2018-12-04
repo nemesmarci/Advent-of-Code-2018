e4b2d838a5318b0d0a658ae3a04e79279b3c6bfe
Marcell Nemes <nemes@sch.bme.hu>
2018-12-05 00:59:14 +0100

    Day 4: Repose Record - part 2

---
+++ 4/4_2.py
@@ -0,0 +1,28 @@
+import re
+import sys
+from collections import defaultdict, Counter
+from datetime import datetime
+
+pattern = re.compile(
+    r"^\[([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2})\] (.*)$")
+
+guard_regex = re.compile(r"^Guard #([0-9]+) .*$")
+
+records = dict()
+for line in sys.stdin.readlines():
+    timestamp, msg = pattern.match(line).groups()
+    records[timestamp] = msg
+guards = defaultdict(Counter)
+for key in sorted(records.keys()):
+    guard = guard_regex.match(records[key])
+    if guard:
+        guard_id = guard.group(1)
+    elif "falls" in records[key]:
+        start = datetime.strptime(key, "%Y-%m-%d %H:%M").minute
+    elif "wakes" in records[key]:
+        end = datetime.strptime(key, "%Y-%m-%d %H:%M").minute
+        for i in range(start, end):
+            guards[guard_id][i] += 1
+
+max_sleep = max(guards, key=lambda g: guards[g].most_common()[0][1])
+print(guards[max_sleep].most_common()[0][0] * int(max_sleep))
