6ec85e64c1e119a7812f86b81190b6c05b2f58fc
Marcell Nemes <nemes@sch.bme.hu>
2019-12-07 18:43:34 +0100

    Refactor day 3

--- 3/3_1.py
+++ 3/3_1.py
@@ -1,16 +1,3 @@
-import sys
-import re
-from collections import defaultdict
+from common import read_data, process
 
-squares = defaultdict(int)
-conflicts = 0
-for line in sys.stdin.readlines():
-    pattern = re.compile(r"^#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)$")
-    num, xcoord, ycoord, width, height = [
-        int(x) for x in pattern.match(line).groups()]
-    for i in range(xcoord, xcoord + width):
-        for j in range(ycoord, ycoord + height):
-            squares[(i, j)] += 1
-            if squares[(i, j)] == 2:
-                conflicts += 1
-print(conflicts)
+print(process(read_data())[0])
