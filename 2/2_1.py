1838392527a6fb1610bb08d7d52c4ce515e138c9
Marcell Nemes <nemes@sch.bme.hu>
2019-12-07 18:43:17 +0100

    Refactor day 2

--- 2/2_1.py
+++ 2/2_1.py
@@ -1,12 +1,13 @@
-import sys
 from collections import Counter
 
 twos = 0
 threes = 0
-for line in sys.stdin.readlines():
-    cnt = Counter(line)
-    if 2 in cnt.values():
-        twos += 1
-    if 3 in cnt.values():
-        threes += 1
+with open('input.txt') as data:
+    for line in data:
+        cnt = Counter(line)
+        if 2 in cnt.values():
+            twos += 1
+        if 3 in cnt.values():
+            threes += 1
+
 print(twos * threes)
