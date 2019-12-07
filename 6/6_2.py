523d5087cea94af879f92c4d09483a04aade6f36
Marcell Nemes <nemes@sch.bme.hu>
2019-12-07 20:37:10 +0100

    Refactor day 6

--- 6/6_2.py
+++ 6/6_2.py
@@ -1,29 +1,15 @@
-import sys
 from collections import defaultdict
+from common import read_data, make_map, distances
 
-xcoords, ycoords = [], []
-lines = sys.stdin.readlines()
-for line in lines:
-    x,y = [int(c) for c in line.split(',')]
-    xcoords.append(x)
-    ycoords.append(y)
-
-coords = [(xcoords[h], ycoords[h]) for h in range(len(xcoords))]
-
-min_x, min_y = min(xcoords), min(ycoords)
-max_x, max_y = max(xcoords), max(ycoords)
+borders, coords = read_data()
+point_to_coord, coord_to_points = make_map(borders, coords)
 
 point_to_sum = defaultdict(int)
+min_x, max_x, min_y, max_y = borders
 
-def distance(x, y):
-    return abs(x[0] - y[0]) + abs(x[1] - y[1])
+for x in range(min_x, max_x + 1):
+    for y in range(min_y, max_y + 1):
+        ls = distances((x, y), coords)
+        point_to_sum[(x, y)] = sum(ls)
 
-def distances(coord, coords):
-    return [distance(c, coord) for c in coords]
-
-for i in range(min_x, max_x + 1):
-    for j in range(min_y, max_y + 1):
-        ls = distances((i,j), coords)
-        point_to_sum[(i,j)] = sum(ls)
-                
-print(len([s for s in point_to_sum.values() if s < 10000]))
+print(len([dist for dist in point_to_sum.values() if dist < 10000]))
