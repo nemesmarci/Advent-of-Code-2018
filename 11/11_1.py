bbb56e2139420204aa5733295f93b56b9eda79c0
Marcell Nemes <nemes@sch.bme.hu>
2019-12-08 20:55:25 +0100

    Refactor day 11

--- 11/11_1.py
+++ 11/11_1.py
@@ -1,33 +1,4 @@
-import sys
-import numpy as np
+from common import read_data, find_max_power
 
-serial = int(sys.stdin.readline().strip())
-
-cells = np.zeros((300, 300), dtype=int)
-
-for i in range(1, 300 + 1):
-    for j in range(1, 300 + 1):
-        rack_id = i + 10
-        power = rack_id * j
-        power += serial
-        power *= rack_id
-        power = str(power)
-        power = int(power[-3]) if len(power) > 2 else 0
-        power -= 5
-        cells[i - 1][j - 1] = power
-
-def stack(x, y):
-    total = 0
-    for i in range(3):
-        for j in range(3):
-            total += cells[x + i][y + j]
-    return total
-
-max_total = None
-for i in range(1, 300 - 2):
-    for j in range(1, 300 - 2):
-        total = stack(i - 1, j - 1)
-        if max_total is None or total > max_total:
-            max_total = total
-            square = (str(i), str(j))
-print(",".join(square))
+cells = read_data()
+print(",".join(find_max_power(cells, [3])[:-1]))
