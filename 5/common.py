77021e01fba6632f1f252884ca980d0cf2273aa8
Marcell Nemes <nemes@sch.bme.hu>
2019-12-08 17:12:42 +0100

    Fix formatting

--- 5/common.py
+++ 5/common.py
@@ -1,3 +1,3 @@
 def react(a, b):
     lhs, ret = (a[-1], a[:-1]) if len(a) > 1 else (a, "")
-    return ret if lhs != b and lhs.lower() == b.lower() else a + b  
+    return ret if lhs != b and lhs.lower() == b.lower() else a + b
