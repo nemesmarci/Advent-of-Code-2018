67d892c66997eb47afb98ae2a88abc9f953acfa4
Marcell Nemes <nemes@sch.bme.hu>
2019-12-08 17:51:33 +0100

    Refactor day 8

--- 8/8_1.py
+++ 8/8_1.py
@@ -1,26 +1,5 @@
-import sys
+from common import read_data, parse
 
-class Node:
-    def __init__(self, childs, meta):
-        self.childs = childs
-        self.meta = meta
-
-def parse_node(n):
-    n_childs = int(line[n])
-    n += 1
-    n_meta = int(line[n])
-    childs = []
-    meta = []
-    for i in range(n_childs):
-        n += 1
-        j, child = parse_node(n)
-        childs.append(child)
-        n += (j - n)
-    for i in range(n_meta):
-        n += 1
-        m = int(line[n])
-        meta.append(m)
-    return (n, Node(childs, meta))
 
 def value(node):
     v = sum(node.meta)
@@ -28,7 +7,6 @@ def value(node):
         v += value(child)
     return v
 
-line = sys.stdin.readline().split()
 
-root = parse_node(0)[1]
+root = parse(0, read_data())[1]
 print(value(root))
--- 8/8_2.py
+++ 8/8_2.py
@@ -1,40 +1,16 @@
-import sys
+from common import read_data, parse
 
-class Node:
-    def __init__(self, childs, meta):
-        self.childs = childs
-        self.meta = meta
 
-def parse_node(n):
-    n_childs = int(line[n])
-    n += 1
-    n_meta = int(line[n])
-    childs = []
-    meta = []
-    for i in range(n_childs):
-        n += 1
-        j, child = parse_node(n)
-        childs.append(child)
-        n += (j - n)
-    for i in range(n_meta):
-        n += 1
-        m = int(line[n])
-        meta.append(m)
-    return (n, Node(childs, meta))
-    
 def value(node):
     v = 0
     if len(node.childs) == 0:
         return sum(node.meta)
     for i in node.meta:
-        if i == 0:
-            continue
-        if i > len(node.childs):
+        if i == 0 or i > len(node.childs):
             continue
         v += value(node.childs[i-1])
     return v
-        
-line = sys.stdin.readline().split()
 
-root = parse_node(0)[1]
+
+root = parse(0, read_data())[1]
 print(value(root))
